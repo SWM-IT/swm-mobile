be8327a5ffb6333ceede4d7aa2012538740789f4
wiese.daniel <wiese.daniel@swm.de>
2014-06-26 15:07:00 +0200

    Refactoring / fixing the build

--- swm-mobile-kitchensink/src/main/java/de/swm/mobile/kitchensink/client/showcase/WidgetsPage.java
+++ swm-mobile-kitchensink/src/main/java/de/swm/mobile/kitchensink/client/showcase/WidgetsPage.java
@@ -1,12 +1,6 @@
 package de.swm.mobile.kitchensink.client.showcase;
 
-import com.google.gwt.core.client.GWT;
-import com.google.gwt.uibinder.client.UiBinder;
-import com.google.gwt.uibinder.client.UiField;
 import com.google.gwt.user.client.ui.HasWidgets;
-import com.google.gwt.user.client.ui.Widget;
-import de.swm.commons.mobile.client.widgets.SimpleHeaderPanel;
-import de.swm.mobile.kitchensink.client.Application;
 import de.swm.mobile.kitchensink.client.base.ShowcaseOverviewPage;
 import de.swm.mobile.kitchensink.client.components.TestResources;
 import de.swm.mobile.kitchensink.client.showcase.widgets.*;
