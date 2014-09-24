214231c80b771593b9e20ed9673f7c8e6538674e
Franziskus Karsunke <karsunke.franziskus@swm.de>
2014-09-24 16:19:35 +0200

    Moved SimpleScrollpanel to package scroll.

--- swm-mobile/src/main/java/de/swm/commons/mobile/client/widgets/scroll/SimpleScrollPanel.java
+++ swm-mobile/src/main/java/de/swm/commons/mobile/client/widgets/scroll/SimpleScrollPanel.java
@@ -13,7 +13,7 @@
  * License for the specific language governing permissions and limitations under
  * the License.
  */
-package de.swm.commons.mobile.client.widgets.experimental;
+package de.swm.commons.mobile.client.widgets.scroll;
 
 import com.google.gwt.core.client.JavaScriptObject;
 import com.google.gwt.core.client.JsArray;
