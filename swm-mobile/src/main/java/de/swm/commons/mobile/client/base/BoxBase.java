90daa5029be0c1686cae4d39aa815c5c68ab99fd
Franziskus Karsunke <karsunke.franziskus@swm.de>
2014-10-09 15:27:59 +0200

    Numbertextboxes now have "tel" as type

--- swm-mobile/src/main/java/de/swm/commons/mobile/client/base/BoxBase.java
+++ swm-mobile/src/main/java/de/swm/commons/mobile/client/base/BoxBase.java
@@ -164,8 +164,8 @@ public class BoxBase<T> extends com.google.gwt.user.client.ui.ValueBoxBase<T> im
         var e = $doc.createElement("INPUT");
         e.type = type;
 		if (type === "number") {
-			e.type = "text";
-            e.pattern = "\d+(\,\d*)?*";
+			e.type = "tel";
+            e.pattern = "[0-9]";
 			e.onkeypress = function isNumber(evt) {
                 evt = (evt) ? evt : window.event;
                 var charCode = (evt.which) ? evt.which : evt.keyCode;
