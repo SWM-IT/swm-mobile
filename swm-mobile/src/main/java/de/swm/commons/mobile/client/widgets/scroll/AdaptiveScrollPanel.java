4d8425dd9d386fbda08630c4c96ae20d1889ee12
Franziskus Karsunke <karsunke.franziskus@swm.de>
2014-11-10 18:00:34 +0100

    Adaptive-Scrollpanel hinzugefügt.

---
+++ swm-mobile/src/main/java/de/swm/commons/mobile/client/widgets/scroll/AdaptiveScrollPanel.java
@@ -0,0 +1,99 @@
+package de.swm.commons.mobile.client.widgets.scroll;
+
+import com.google.gwt.dom.client.Style;
+import com.google.gwt.user.client.ui.Widget;
+import de.swm.commons.mobile.client.SWMMobile;
+import de.swm.commons.mobile.client.base.PanelBase;
+
+/**
+ * This Scrollpanel detects which OS it is running on and shows the SimpleScrollPanelWithScrollbar for iOS
+ * and the SimpleScrollPanel for all Other OSes.
+ *
+ * @author karsunke.franziskus
+ *         <br>
+ *         copyright (C) 10.11.2014, Stadtwerke München GmbH
+ */
+public class AdaptiveScrollPanel extends PanelBase implements IScrollPanel {
+
+	private IScrollPanel realPanel;
+
+	public AdaptiveScrollPanel() {
+	}
+
+	private void createScrollPanelIfNeeded() {
+		if (realPanel == null) {
+			if (SWMMobile.getOsDetection().isIOs()) {
+				ScrollPanelWithScrollbar scrollPanelWithScrollbar = new ScrollPanelWithScrollbar();
+				super.add(scrollPanelWithScrollbar);
+				this.realPanel = scrollPanelWithScrollbar;
+			} else {
+				SimpleScrollPanel scrollPanel = new SimpleScrollPanel();
+				super.add(scrollPanel);
+				this.realPanel = scrollPanel;
+			}
+			this.getElement().getStyle().setHeight(100.0, Style.Unit.PCT);
+		}
+	}
+
+	@Override
+	public void onLoad() {
+		super.onLoad();
+		createScrollPanelIfNeeded();
+		((PanelBase) realPanel).getElement().getStyle().setHeight(
+				this.getElement().getOffsetHeight() - 50, Style.Unit.PX);
+	}
+
+	@Override
+	public void setScrollMonitor(IScrollMonitor scrollMonitor) {
+		createScrollPanelIfNeeded();
+		this.realPanel.setScrollMonitor(scrollMonitor);
+	}
+
+	@Override
+	public void setPostionToTop() {
+		createScrollPanelIfNeeded();
+		this.realPanel.setPostionToTop();
+	}
+
+	@Override
+	public void setPositionToBottom() {
+		createScrollPanelIfNeeded();
+		this.realPanel.setPositionToBottom();
+	}
+
+	@Override
+	public void setScrollPosition(int pos) {
+		createScrollPanelIfNeeded();
+		this.realPanel.setScrollPosition(pos);
+	}
+
+	@Override
+	public int getScrollPosition() {
+		createScrollPanelIfNeeded();
+		return this.realPanel.getScrollPosition();
+	}
+
+	@Override
+	public int getScrollToPosition() {
+		createScrollPanelIfNeeded();
+		return this.realPanel.getScrollToPosition();
+	}
+
+	@Override
+	public void setOffsetHeight(int offsetHeight) {
+		createScrollPanelIfNeeded();
+		this.realPanel.setOffsetHeight(offsetHeight);
+	}
+
+	@Override
+	public void add(Widget w) {
+		createScrollPanelIfNeeded();
+		((PanelBase) realPanel).add(w);
+	}
+
+	@Override
+	public void clear() {
+		createScrollPanelIfNeeded();
+		((PanelBase) realPanel).clear();
+	}
+}
