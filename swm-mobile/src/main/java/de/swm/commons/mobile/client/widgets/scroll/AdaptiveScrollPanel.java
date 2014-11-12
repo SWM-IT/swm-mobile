7c6fbfd6a47fd209e9e32b5109b03c3d3231a91a
Franziskus Karsunke <karsunke.franziskus@swm.de>
2014-11-12 16:33:10 +0100

    fixed some css-issues in Adaptive-Scrollpanel

--- swm-mobile/src/main/java/de/swm/commons/mobile/client/widgets/scroll/AdaptiveScrollPanel.java
+++ swm-mobile/src/main/java/de/swm/commons/mobile/client/widgets/scroll/AdaptiveScrollPanel.java
@@ -1,10 +1,14 @@
 package de.swm.commons.mobile.client.widgets.scroll;
 
 import com.google.gwt.dom.client.Style;
+import com.google.gwt.user.client.ui.Composite;
+import com.google.gwt.user.client.ui.Panel;
 import com.google.gwt.user.client.ui.Widget;
 import de.swm.commons.mobile.client.SWMMobile;
 import de.swm.commons.mobile.client.base.PanelBase;
 import de.swm.commons.mobile.client.widgets.toolbar.ToolbarPanel;
+
+import java.util.Iterator;
 
 /**
  * This Scrollpanel detects which OS it is running on and shows the SimpleScrollPanelWithScrollbar for iOS
@@ -18,31 +22,26 @@ public class AdaptiveScrollPanel extends PanelBase implements IScrollPanel {
 
 	private IScrollPanel realPanel;
 
-	public AdaptiveScrollPanel() {
-	}
-
 	private void createScrollPanelIfNeeded() {
-		if (realPanel == null) {
-			if (SWMMobile.getOsDetection().isIOs()) {
-				ScrollPanelWithScrollbar scrollPanelWithScrollbar = new ScrollPanelWithScrollbar();
-				super.add(scrollPanelWithScrollbar);
-				this.realPanel = scrollPanelWithScrollbar;
-			} else {
-				SimpleScrollPanel scrollPanel = new SimpleScrollPanel();
-				super.add(scrollPanel);
-				scrollPanel.getElement().getStyle().setOverflowY(Style.Overflow.HIDDEN);
-				this.getElement().getStyle().setOverflowY(Style.Overflow.SCROLL);
-				this.realPanel = scrollPanel;
-			}
-			this.getElement().getStyle().setProperty("WebkitBoxFlex", "1");
-			this.setHeight("100%");
+		if (SWMMobile.getOsDetection().isIOs()) {
+			ScrollPanelWithScrollbar scrollPanelWithScrollbar = new ScrollPanelWithScrollbar();
+			this.getElement().getStyle().setProperty("display", "-webkit-box");
+			this.realPanel = scrollPanelWithScrollbar;
+		} else {
+			SimpleScrollPanel scrollPanel = new SimpleScrollPanel();
+			scrollPanel.getElement().getStyle().setOverflowY(Style.Overflow.HIDDEN);
+			this.getElement().getStyle().setOverflowY(Style.Overflow.SCROLL);
+			this.realPanel = scrollPanel;
 		}
+		super.add((PanelBase) realPanel);
+		this.getElement().getStyle().setProperty("WebkitBoxFlex", "1");
+		this.setHeight("100%");
 	}
 
 	@Override
 	public void onLoad() {
 		super.onLoad();
-		createScrollPanelIfNeeded();
+		this.createScrollPanelIfNeeded();
 	}
 
 	@Override
@@ -97,5 +96,17 @@ public class AdaptiveScrollPanel extends PanelBase implements IScrollPanel {
 	public void clear() {
 		createScrollPanelIfNeeded();
 		((PanelBase) realPanel).clear();
+	}
+
+	@Override
+	public Iterator<Widget> iterator() {
+		createScrollPanelIfNeeded();
+		return ((PanelBase) realPanel).iterator();
+	}
+
+	@Override
+	public boolean remove(Widget widget) {
+		createScrollPanelIfNeeded();
+		return ((PanelBase) realPanel).remove(widget);
 	}
 }
